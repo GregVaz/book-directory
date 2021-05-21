fbb33c4458577649f5e8209d6bbe823b5fc452ec
Gregorio <gregoriovazya@gmail.com>
2021-05-21 10:12:52 -0600

    Add middleware structure and attend comments

--- routes/index.mjs
+++ routes/index.mjs
@@ -2,24 +2,13 @@ import { default as express } from 'express';
 import passport from 'passport';
 export const router = express.Router();
 import { getAllBooks, getRegister, getLogin, logout } from '../controllers/index.mjs';
+import { validateSession, redirectToActiveSession } from '../middleware/index.mjs';
 
-router.get('/', (req, res, next) => {
-  if (req.isAuthenticated()) return next();
+router.get('/', validateSession, getAllBooks);
 
-  res.redirect('/login');
-}, getAllBooks);
+router.get('/login', redirectToActiveSession, getLogin);
 
-router.get('/login', (req, res, next) => {
-  if (req.isAuthenticated()) res.redirect('/');
-
-  return next();
-}, getLogin);
-
-router.get('/register', (req, res, next) => {
-  if (req.isAuthenticated()) res.redirect('/');
-  
-  return next();
-}, getRegister);
+router.get('/register', redirectToActiveSession, getRegister);
 
 router.post('/login',
   passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' })
