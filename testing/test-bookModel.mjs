34fbd57f21ca9802e5dc11bd2968c4f69caf8315
Gregorio <gregoriovazya@gmail.com>
2021-05-25 10:12:57 -0600

    Testing CRUD book model

--- testing/test-bookModel.mjs
+++ testing/test-bookModel.mjs
@@ -2,6 +2,7 @@ import util from 'util';
 import Chai from 'chai';
 import { useBookModel as useBookModel } from '../models/books-store.mjs';
 import { useUserModel as useUserModel } from '../models/books-store.mjs';
+import { Book } from '../models/books.mjs';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -62,6 +63,104 @@ describe('Model Book Test', function() {
       for (let key of keys) {
         assert.match(key, /[0-9]{1,3}/)
       }
+    });
+
+    after(async function() {
+      const keyz = await bookStore.keylist(testUser.email);
+      for (let key of keyz) {
+        await bookStore.destroy(key);
+      }
+    });
+  });
+
+  context('read and create a book', function() {
+    let bookTest1, bookTest2;
+    before(async function() {
+      bookTest1 = await bookStore.create('title1', 'author1', new Date().toISOString(), 'abstract1', 'cover1', testUser.email);
+      bookTest2 = await bookStore.create('title2', 'author2', new Date().toISOString(), 'abstract2', 'cover2', testUser.email);
+    });
+
+    it('should have proper bookTest1', async function() {
+      const book = await bookStore.read(bookTest1.id);
+      assert.exists(book);
+      assert.deepEqual(book, bookTest1);
+    });
+
+    it('should have proper bookTest2', async function() {
+      const book = await bookStore.read(bookTest2.id);
+      assert.exists(book);
+      assert.deepEqual(book, bookTest2);
+    });
+
+    it('Unknown book should fail', async function() {
+      try {
+        const book = await bookStore.read('badkey12');
+        assert.notExists(book);
+      } catch(err) {
+        assert.equal(err.message, 'Cannot read property \'id\' of null');
+      }
+    });
+
+    after(async function() {
+      const keyz = await bookStore.keylist(testUser.email);
+      for (let key of keyz) {
+        await bookStore.destroy(key);
+      }
+    });
+  });
+
+  context('delete a book', function() {
+    let bookTest1;
+    before(async function() {
+      bookTest1 = await bookStore.create('title1', 'author1', new Date().toISOString(), 'abstract1', 'cover1', testUser.email);
+    });
+
+    it('should have proper bookTest1', async function() {
+      const book = await bookStore.read(bookTest1.id);
+      assert.exists(book);
+      assert.deepEqual(book, bookTest1);
+    });
+
+    it('should delete and check if the book exists', async function() {
+      let book = await bookStore.read(bookTest1.id);
+      assert.exists(book);
+      await bookStore.destroy(bookTest1.id);
+      try {
+        const book = await bookStore.read(bookTest1.id);
+        assert.notExists(book);
+      } catch(err) {
+        assert.equal(err.message, `Cannot read property \'id\' of null`);
+      }
+    });
+
+    after(async function() {
+      const keyz = await bookStore.keylist(testUser.email);
+      for (let key of keyz) {
+        await bookStore.destroy(key);
+      }
+    });
+  });
+
+  context('update a book', function() {
+    let bookTest1;
+    before(async function() {
+      bookTest1 = await bookStore.create('title1', 'author1', new Date().toISOString(), 'abstract1', 'cover1', testUser.email);
+    });
+
+    it('should have proper book', async function() {
+      const book = await bookStore.read(bookTest1.id);
+      assert.exists(book);
+      assert.deepEqual(book, bookTest1);
+    });
+
+    it('should update and compare the new data', async function() {
+      let book = await bookStore.read(bookTest1.id);
+      assert.exists(book);
+      await bookStore.update(book.id, book.title, 'authorUpdated', book.publication_date, book.abstract, book.cover);
+      book = await bookStore.read(bookTest1.id);
+      assert.notEqual(book, bookTest1);
+      const bookUpdated = new Book(book.id, book.title, 'authorUpdated', book.publication_date, book.abstract, book.cover, book.userId);
+      assert.deepEqual(book, bookUpdated);
     });
 
     after(async function() {
